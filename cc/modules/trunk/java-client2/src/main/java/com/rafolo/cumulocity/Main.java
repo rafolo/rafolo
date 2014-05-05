135aaefc3acaa1bd8c7c26560a3e0200c17258bf
Rafal Kukuczka <rafal@pkey.pl>
2014-05-05 14:10:00 +0200

    .

--- cc/modules/trunk/java-client2/src/main/java/com/rafolo/cumulocity/Main.java
+++ cc/modules/trunk/java-client2/src/main/java/com/rafolo/cumulocity/Main.java
@@ -28,21 +28,16 @@ public static void main(String[] args) {
     platform.getInventoryApi().getManagedObjects().get();
 
     //To get alarms raised by managed object: 
-// platform.getAlarmApi().getAlarmsByFilter(new AlarmFilter().bySource(managedObjectID));
-
+    // platform.getAlarmApi().getAlarmsByFilter(new AlarmFilter().bySource(managedObjectID));
     //to see them on a map you have to add to your managed object 
 
     c8y.Position p = new c8y.Position();
-
     p.setLat(new BigDecimal(19));
-
     p.setLng(new BigDecimal(49));
-
     p.setAlt(new BigDecimal(300));
 
     mo.set(p);
    
-
     inventory.update(mo);
 }
 }
