58cb8fd301c5820bb9407c1c943458c8ff599426
Olgierd Falat <olgierd.falat@gmail.com>
2014-06-05 19:27:58 +0200

    Adding Jamine unit tests.

--- sample_app/app/assets/javascripts/modules/mappoint/mappoint.js
+++ sample_app/app/assets/javascripts/modules/mappoint/mappoint.js
@@ -5,50 +5,6 @@ var mapPointModule = angular.module('app.mappoint', [])
             controller: 'LegendController'
         });
     })
-    .controller("LegendController", ['$scope', '$log', function ($scope, $log) {
-        $log.warn('Halo2');
-        angular.extend($scope, {
-            amsterdam: {
-                lat: 52.35,
-                lng: 4.91,
-                zoom: 12
-            },
-            legend: {
-                position: 'bottomleft',
-                colors: [ 'red', '#28c9ff', '#0000ff', '#ecf386' ],
-                labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
-            },
-            defaults: {
-                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
-                scrollWheelZoom: true
-            }
-        });
-
-        angular.extend($scope, {
-            osloCenter: {
-                lat: 59.91,
-                lng: 10.75,
-                zoom: 12
-            },
-            markers: {
-                osloMarker: {
-                    lat: 59.91,
-                    lng: 10.75,
-                    message: "I want to travel here!",
-                    focus: true,
-                    draggable: false
-                },
-                osloMarker2: {
-                    lat: 69.91,
-                    lng: 10.75,
-                    message: "I want to travel here!",
-                    focus: true,
-                    draggable: false
-                }
-            },
-            defaults: {
-                scrollWheelZoom: false
-            }
-        });
-
+    .controller("LegendController", ['$scope', '$log', 'mappointService', function ($scope, $log, mappointService) {
+        angular.extend($scope, mappointService.getData());
     }]);
