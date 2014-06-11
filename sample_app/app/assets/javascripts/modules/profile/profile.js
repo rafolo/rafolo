0e0a47eb0a36533066b00074bb0729344230f26a
Rafal Kukuczka <rafal@pkey.pl>
2014-06-11 07:43:26 +0200

    Profiles layouted

--- sample_app/app/assets/javascripts/modules/profile/profile.js
+++ sample_app/app/assets/javascripts/modules/profile/profile.js
@@ -1,6 +1,10 @@
 var profileModule = angular.module('app.profile', [])
     .config(function ($routeProvider) {
-        $routeProvider.when('/profile', {
+        $routeProvider.when('/alarms', {
+            templateUrl: '/assets/modules/profile/alarms.view.html', controller: 'ProfileController'
+        }).when('/devices', {
+            templateUrl: '/assets/modules/profile/devices.view.html', controller: 'ProfileController'
+        }).when('/profile', {
             templateUrl: '/assets/modules/profile/profile.view.html', controller: 'ProfileController'
         }).when('/messages', {
             templateUrl: '/assets/modules/profile/messages.view.html', controller: 'MessageController'
@@ -8,11 +12,11 @@ var profileModule = angular.module('app.profile', [])
 
 
     })
-    .controller("ProfileController", ['$scope', '$log', '$interval','profileService', function ($scope, $log, $interval, profileService) {
+    .controller("ProfileController", ['$scope', '$log', '$interval', 'profileService', function ($scope, $log, $interval, profileService) {
         $log.info("OK, I'm in, inn keeper");
         angular.extend($scope, profileService.getData());
     }])
-    .controller("MessageController", ['$scope', '$log', '$interval','profileService', function ($scope, $log, $interval, profileService) {
+    .controller("MessageController", ['$scope', '$log', '$interval', 'profileService', function ($scope, $log, $interval, profileService) {
         $log.info("OK, I'm in, inn keeper");
         angular.extend($scope, profileService.getData());
     }]);
