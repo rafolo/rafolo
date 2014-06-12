e5fe8ac91d647fe2036c2f47b2e5d2dfb4b5559d
Olgierd Falat <olgierd.falat@gmail.com>
2014-06-12 17:50:46 +0200

    Moved all client dependencies to Bower.

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -10,7 +10,35 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery/dist/jquery
+//= require jquery/jquery
+//= require jqueryui/ui/jquery-ui
+//= require bootstrap/dist/js/bootstrap
+//= require bootstrap-datepicker/js/bootstrap-datepicker
+//= require moment/moment
+//= require fullcalendar/dist/fullcalendar
+//= require jquery.gritter/js/jquery.gritter
+//= require jquery.sparkline/index
+//= require jquery.tagsinput/jquery.tagsinput
+//= require jquery.uniform/jquery.uniform
+//= require iCheck/icheck
+//= require select2/select2
+//= require d3/d3.v2
+//= require bower-xcharts/xcharts
+//= require wysihtml5/dist/wysihtml5-0.3.0
+//= require bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2
+//= require raphael/raphael
+//= require justgage/justgage
+//= require bootstrap-application-wizard/src/bootstrap-wizard
+//= require jquery.easy-pie-chart/jquery.easy-pie-chart
+//= require touchTouch/index
+//= require datatables/media/js/jquery.dataTables
+//= require responsive-tables/index
+//= require validationEngine/js/languages/jquery.validationEngine-en
+//= require validationEngine/js/jquery.validationEngine
+//= require x-editable/dist/bootstrap3-editable/js/bootstrap-editable
+//= require jquery.easing/index
+//= require jquery.metadata/index
+//= require jquery.ibutton/index
 //= require angular/angular
 //= require angular-route/angular-route
 //= require modernizr/modernizr
@@ -20,6 +48,14 @@
 //= require app.main
 //= require leaflet-dist/leaflet
 //= require angular-leaflet-directive/dist/angular-leaflet-directive
+//= require core_admin/gritter
+//= require core_admin/calendar
+//= require charts/xcharts_left_sine
+//= require charts/xcharts_sine
+//= require charts/xcharts_bar
+//= require charts/sparkline_samples
+
 //= #require debug/live-js/live-js #RK added on will
+
 //= require_tree .
 
--- sample_app/config/environments/production.rb
+++ sample_app/config/environments/production.rb
@@ -12,7 +12,7 @@ SampleApp::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   config.assets.compile = true
 
