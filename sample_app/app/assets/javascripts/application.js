a90b5075240e07885bed5aee577a514545828b5f
Olgierd Falat <olgierd.falat@gmail.com>
2014-06-12 18:28:15 +0200

    Fixed Karma.

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery/jquery
+//= require jquery/dist/jquery
 //= require jqueryui/ui/jquery-ui
 //= require bootstrap/dist/js/bootstrap
 //= require bootstrap-datepicker/js/bootstrap-datepicker
