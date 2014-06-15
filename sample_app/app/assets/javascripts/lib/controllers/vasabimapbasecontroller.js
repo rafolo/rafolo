0e0507583a4f2796b124a919811cab943280e641
Rafal <rafal@pkey.pl>
2014-06-15 22:25:21 +0200

    Refactored

---
+++ sample_app/app/assets/javascripts/lib/controllers/vasabimapbasecontroller.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Rafal on 2014-06-15.
+ */
+function VasabiMapBaseController($scope) {
+    var _this = this;
+
+    //        var centerX=59.92;
+//        var centerY = 10.75;
+//        var radius = 0.01;
+//        angular.extend($scope, mappointService.getData(centerX, centerY));
+//
+//        var steps = 10, i=0;
+//        function handler()
+//        {
+//            $log.info('Running handler');
+//            var xValue = (centerX + radius * Math.cos(2 * Math.PI * i / steps));
+//            var yValue = (centerY + radius * Math.sin(2 * Math.PI * i / steps));
+//            angular.extend($scope, mappointService.getData(xValue, yValue));
+//            i+=1;
+//        }
+//
+//        $interval(handler, 1000 );
+
+}
--- sample_app/app/assets/javascripts/modules/alarm/alarm.js
+++ sample_app/app/assets/javascripts/modules/alarm/alarm.js
@@ -7,9 +7,6 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
     }])
     .controller("AlarmController", ['$scope', '$log', '$interval', '$http', 'alarmService', 'StatusesConstant', function ($scope, $log, $interval, $http, mappointService, StatusesConstant) {
 
-        $scope.$http = $http;
-        angular.extend(this, new VasabiGridBaseController($scope));
-
         //Columns
         $scope.statuses = StatusesConstant;
         $scope.cellInputEditableTemplate = '<input ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="updateEntity(row)" />';
@@ -31,160 +28,11 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
             {name: "Young", age: 10}
         ];
 
+        $scope.$http = $http; //TODO! remove and injext services in VGBC
+        angular.extend(this, new VasabiGridBaseController($scope));
+        angular.extend(this, new VasabiChartBaseController($scope));
+        angular.extend(this, new VasabiMapBaseController($scope));
 
-
-
-
-        //chart
-        var data = [
-            {
-                "xScale": "ordinal",
-                "comp": [],
-                "main": [
-                    {
-                        "className": ".main.l1",
-                        "data": [
-                            { "y": 15, "x": "2012-11-19T00:00:00" },
-                            { "y": 11, "x": "2012-11-20T00:00:00" },
-                            { "y": 8, "x": "2012-11-21T00:00:00" },
-                            { "y": 10, "x": "2012-11-22T00:00:00" },
-                            { "y": 1, "x": "2012-11-23T00:00:00" },
-                            { "y": 6, "x": "2012-11-24T00:00:00" },
-                            { "y": 8, "x": "2012-11-25T00:00:00" }
-                        ]
-                    },
-                    {
-                        "className": ".main.l2",
-                        "data": [
-                            {"y": 29, "x": "2012-11-19T00:00:00"},
-                            {"y": 33, "x": "2012-11-20T00:00:00"},
-                            {"y": 13, "x": "2012-11-21T00:00:00"},
-                            {"y": 16, "x": "2012-11-22T00:00:00"},
-                            {"y": 7, "x": "2012-11-23T00:00:00"},
-                            {"y": 18, "x": "2012-11-24T00:00:00"},
-                            {"y": 8, "x": "2012-11-25T00:00:00"}
-                        ]
-                    }
-                ],
-                "type": "line-dotted",
-                "yScale": "linear"
-            },
-            {
-                "xScale": "ordinal",
-                "comp": [],
-                "main": [
-                    {
-                        "className": ".main.l1",
-                        "data": [
-                            {"y": 12, "x": "2012-11-19T00:00:00"},
-                            {"y": 18, "x": "2012-11-20T00:00:00"},
-                            {"y": 8, "x": "2012-11-21T00:00:00"},
-                            {"y": 7, "x": "2012-11-22T00:00:00"},
-                            {"y": 6, "x": "2012-11-23T00:00:00"},
-                            {"y": 12, "x": "2012-11-24T00:00:00"},
-                            {"y": 8, "x": "2012-11-25T00:00:00"}
-                        ]
-                    },
-                    {
-                        "className": ".main.l2",
-                        "data": [
-                            {"y": 29, "x": "2012-11-19T00:00:00"},
-                            {"y": 33, "x": "2012-11-20T00:00:00"},
-                            {"y": 13, "x": "2012-11-21T00:00:00"},
-                            {"y": 16, "x": "2012-11-22T00:00:00"},
-                            {"y": 7, "x": "2012-11-23T00:00:00"},
-                            {"y": 18, "x": "2012-11-24T00:00:00"},
-                            {"y": 8, "x": "2012-11-25T00:00:00"}
-                        ]
-                    }
-                ],
-                "type": "cumulative",
-                "yScale": "linear"
-            },
-            {
-                "xScale": "ordinal",
-                "comp": [],
-                "main": [
-                    {
-                        "className": ".main.l1",
-                        "data": [
-                            {"y": 12, "x": "2012-11-19T00:00:00"},
-                            {"y": 18, "x": "2012-11-20T00:00:00"},
-                            {"y": 8, "x": "2012-11-21T00:00:00"},
-                            {"y": 7, "x": "2012-11-22T00:00:00"},
-                            {"y": 6, "x": "2012-11-23T00:00:00"},
-                            {"y": 12, "x": "2012-11-24T00:00:00"},
-                            {"y": 8, "x": "2012-11-25T00:00:00"}
-                        ]
-                    },
-                    {
-                        "className": ".main.l2",
-                        "data": [
-                            {"y": 29, "x": "2012-11-19T00:00:00"},
-                            {"y": 33, "x": "2012-11-20T00:00:00"},
-                            {"y": 13, "x": "2012-11-21T00:00:00"},
-                            {"y": 16, "x": "2012-11-22T00:00:00"},
-                            {"y": 7, "x": "2012-11-23T00:00:00"},
-                            {"y": 18, "x": "2012-11-24T00:00:00"},
-                            {"y": 8, "x": "2012-11-25T00:00:00"}
-                        ]
-                    }
-                ],
-                "type": "bar",
-                "yScale": "linear"
-            }
-        ];
-
-        var order = [0, 1, 0, 2],
-            i = 0,
-            xFormat = d3.time.format('%A'),
-            rotateTimer,
-            chart,
-            t = 3500;
-
-        if ($("#xchart-sine2").length > 0) {
-            chart = new xChart('bar', data[order[i]], '#xchart-sine2', {
-                axisPaddingTop: 5,
-                paddingLeft: 30,
-                dataFormatX: function (x) {
-                    return new Date(x);
-                },
-                tickFormatX: function (x) {
-                    return d3.time.format('%a')(x);
-                }
-            });
-
-            rotateTimer = setTimeout(rotateChart, t);
-        }
-
-        function updateChart(i) {
-            chart.setData(data[i]);
-        }
-
-        function rotateChart() {
-            i += 1;
-            i = (i >= order.length) ? 0 : i;
-            updateChart(order[i]);
-            rotateTimer = setTimeout(rotateChart, t);
-        }
-
-
-//        var centerX=59.92;
-//        var centerY = 10.75;
-//        var radius = 0.01;
-//        angular.extend($scope, mappointService.getData(centerX, centerY));
-//
-//        var steps = 10, i=0;
-//        function handler()
-//        {
-//            $log.info('Running handler');
-//            var xValue = (centerX + radius * Math.cos(2 * Math.PI * i / steps));
-//            var yValue = (centerY + radius * Math.sin(2 * Math.PI * i / steps));
-//            angular.extend($scope, mappointService.getData(xValue, yValue));
-//            i+=1;
-//        }
-//
-//        $interval(handler, 1000 );
     }]).directive('ngBlur', function () {
         return function (scope, elem, attrs) {
             elem.bind('blur', function () {
