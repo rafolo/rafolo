1ccda80bcaab66c6bd6d5b5b2714196941974653
Rafal <rafal@pkey.pl>
2014-06-15 22:33:29 +0200

    Refactored

--- sample_app/app/assets/javascripts/modules/alarm/alarm.js
+++ sample_app/app/assets/javascripts/modules/alarm/alarm.js
@@ -7,6 +7,7 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
     }])
     .controller("AlarmController", ['$scope', '$log', '$interval', '$http', 'alarmService', 'StatusesConstant', function ($scope, $log, $interval, $http, mappointService, StatusesConstant) {
 
+        //GRID
         //Columns
         $scope.statuses = StatusesConstant;
         $scope.cellInputEditableTemplate = '<input ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="updateEntity(row)" />';
@@ -16,18 +17,26 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
             { field: 'name', displayName: 'Name', enableCellEditOnFocus: true,
                 editableCellTemplate: $scope.cellInputEditableTemplate, colFilterText: '' },
             { field: 'age', displayName: 'Age', enableCellEdit: false },
-            { field: 'born', displayName: 'Born', enableCellEdit: false },
+            { field: 'born', displayName: 'Born', enableCellEdit: false, cellFilter: 'datetime' },
             { field: 'status', displayName: 'Status', enableCellEditOnFocus: true,
                 editableCellTemplate: $scope.cellSelectEditableTemplate,
-                cellFilter: 'mapStatus'}
+                cellFilter: 'status'}
         ];
 
-        //Combo
+        //Init
+        $scope.$on('ngGridEventData', function () {
+            $scope.gridOptions.selectRow(0, true);
+        });
+        //GRID/
+
+        //COMBO
         $scope.myComboData = [
             {name: "Old", age: 50},
             {name: "Young", age: 10}
         ];
+        //COMBO/
 
+        //Internals - do not touch
         $scope.$http = $http; //TODO! remove and injext services in VGBC
         angular.extend(this, new VasabiGridBaseController($scope));
         angular.extend(this, new VasabiChartBaseController($scope));
@@ -41,13 +50,26 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
             });
         };
     })
-    .filter('mapStatus', function (StatusesConstant) {
+    .filter('status', function (StatusesConstant) {
         return function (input) {
             if (StatusesConstant[input]) {
                 return StatusesConstant[input];
             } else {
                 return 'unknown';
             }
+        };
+    })
+    .filter('datetime', function ($filter) {
+        return function (input) {
+            if (input == null) {
+                return "";
+            }
+
+            var _date = $filter('date')(new Date(input),
+                'MMM dd yyyy - HH:mm:ss');
+
+            return _date.toUpperCase();
+
         };
     })
     .factory('StatusesConstant', function () {
