f00e240ef77fdee3ce98b34914e2d185a3f50c47
Rafal <rafal@pkey.pl>
2014-06-16 14:03:38 +0200

    Refactor 1

--- sample_app/app/assets/javascripts/lib/directives/tables.js
+++ sample_app/app/assets/javascripts/lib/directives/tables.js
@@ -1,11 +1,104 @@
 var libDirectives = angular.module("lib.directives", [])
-    .directive("vasabiGrid", function () {
+    .directive('customGrid', function ($compile) {
+
         return {
-            restrict: "A",
-            transclude: true,
-            templateUrl: '/assets/lib/directives/base/vasabi-grid.html'
+            restrict: 'E',
+            templateUrl: '/assets/lib/directives/base/grid.html',
+            scope: { items: '=', cols: '=', selectedItems: '=', customOptions: '=', pagingOptions: '='},
+            replace: true,
+            transclude: false,
+            controller: controller
         };
-    }).directive("vasabiAlarm", function () {
+
+        function controller($scope, $attrs) {
+            $scope.selectedItems = [];
+
+            var customOptions = $scope.customOptions;
+
+            var fixedOptions = {
+                columnDefs: 'cols',
+                data: 'items'
+            };
+
+            //defaultOptions
+            var pagingOptions = $scope.pagingOptions; //TODO! move to custom options
+            var enablePaging = false;
+            if (pagingOptions){
+                enablePaging = true;
+            }
+            var defaultOptions = {
+                selectedItems: $scope.selectedItems,
+                showSelectionCheckbox: true,
+                showFooter: true,
+                filterOptions: {
+                    filterText: '',
+                    useExternalFilter: false
+                },
+                multiSelect: false,
+                enableCellEditOnFocus: true,
+                enablePaging: enablePaging,
+                pagingOptions: pagingOptions,
+                showFooter: true,
+                totalServerItems: 'totalServerItems'
+
+            };
+
+            $scope.options = {};
+
+            angular.extend($scope.options, defaultOptions);
+            angular.extend($scope.options, customOptions);
+            angular.extend($scope.options, fixedOptions);
+
+            $scope.$watch('search', function (value) {
+                $scope.options.filterOptions.filterText = value;
+            });
+
+
+        };
+    })
+//    .directive("vasabiGrid", function () {
+//        return {
+//            restrict: "E",
+//            templateUrl: '/assets/lib/directives/base/vasabi-grid.html',
+//            scope: { items: '=', cols: '=', selectedItems: '=', customOptions: '='},
+//            replace: true,
+//            transclude: true,
+//            controller: controller
+//        };
+//
+//        function controller($scope, $attrs) {
+//            $scope.selectedItems = [];
+//
+//
+//
+//            var customOptions = $scope.customOptions;
+//
+//            var fixedOptions = {
+//                columnDefs: 'cols',
+//                data: 'items'
+//            };
+//
+//            var defaultOptions = {
+//                selectedItems: $scope.selectedItems,
+//                showSelectionCheckbox: true,
+//                showFooter: true,
+//                filterOptions: {
+//                    filterText: '',
+//                    useExternalFilter: false
+//                }
+//            };
+//
+//            $scope.options = {};
+//            angular.extend($scope.options, defaultOptions);
+//            angular.extend($scope.options, customOptions);
+//            angular.extend($scope.options, fixedOptions);
+//
+//            $scope.$watch('search', function (value) {
+//                $scope.options.filterOptions.filterText = value;
+//            });
+//        }
+//    })
+    .directive("vasabiAlarm", function () {
         return {
             restrict: "A",
             transclude: true,
--- sample_app/app/assets/javascripts/modules/alarm/alarm.js
+++ sample_app/app/assets/javascripts/modules/alarm/alarm.js
@@ -7,12 +7,12 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
     }])
     .controller("AlarmController", ['$scope', '$log', '$interval', '$http', 'alarmService', 'StatusesConstant', function ($scope, $log, $interval, $http, mappointService, StatusesConstant) {
 
-        //GRID
-        //Columns
+        //test
+        //1
+        $scope.persons = [];
         $scope.statuses = StatusesConstant;
         $scope.cellInputEditableTemplate = '<input ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="updateEntity(row)" />';
         $scope.cellSelectEditableTemplate = '<select ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-options="id as name for (id, name) in statuses" ng-blur="updateEntity(row)" />';
-
         $scope.columnDefs = [
             { field: 'name', displayName: 'Name', enableCellEditOnFocus: true,
                 editableCellTemplate: $scope.cellInputEditableTemplate, colFilterText: '' },
@@ -23,24 +23,128 @@ var alarmModule = angular.module('app.alarm', ['lib.directives'])
                 cellFilter: 'status'}
         ];
 
+        //Pagination
+        $scope.totalServerItems = 0;
+        $scope.pagingOptions = {
+            pageSizes: [5, 10, 50],
+            pageSize: 10,
+            currentPage: 1
+        };
+
+        $scope.setPagingData = function (data, page, pageSize) {
+            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.persons = pagedData;
+            $scope.totalServerItems = data.length;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+        $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+            setTimeout(function () {
+                var data;
+                if (searchText) {
+                    var ft = searchText.toLowerCase();
+                    $scope.$http.get('/largeLoad.json').success(function (largeLoad) {
+                        data = largeLoad.filter(function (item) {
+                            return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                        });
+                        $scope.setPagingData(data, page, pageSize);
+                    });
+                } else {
+                    $scope.$http.get('/largeLoad.json').success(function (largeLoad) {
+                        $scope.setPagingData(largeLoad, page, pageSize);
+                    });
+                }
+            }, 100);
+        };
+
+        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+        $scope.$watch($scope.pagingOptions, function (newVal, oldVal) {
+            if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+            }
+        }, true);
+
+        //Save
+        $scope.updateEntity = function (row) {
+
+            if (!$scope.save) {
+                $scope.save = { promise: null, pending: false, row: null };
+            }
+            $scope.save.row = row.rowIndex;
+            if (!$scope.save.pending) {
+                $scope.save.pending = true;
+                $scope.save.promise = $timeout(function () {
+                    // $scope.list[$scope.save.row].$update();
+                    console.log("Here you'd save your record to the server, we're updating row: "
+                        + $scope.save.row + " to be: "
+                        + $scope.myData[$scope.save.row].name + ","
+                        + $scope.myData[$scope.save.row].age + ","
+                        + $scope.myData[$scope.save.row].status);
+                    $scope.save.pending = false;
+                }, 500);
+            }
+        };
+
         //Init
         $scope.$on('ngGridEventData', function () {
             $scope.gridOptions.selectRow(0, true);
         });
-        //GRID/
 
-        //COMBO
+
+
+        //2
+        $scope.persons2 = [{name: "Person2", age: 50},
+            {name: "Person2", age: 43},
+            {name: "Person2", age: 27},
+            {name: "Person2", age: 29},
+            {name: "Person2", age: 34}];
+
+        $scope.columnDefs2= [{field: 'name', displayName: 'Name'}, {field:'age', displayName:'Age'}];
+
+
+//        $scope.gridOptions = {
+//            showColumnMenu : true,
+//            showGroupPanel : true
+//        };
+        //
+
+
+//        //GRID
+//        //Columns
+//        $scope.statuses = StatusesConstant;
+//        $scope.cellInputEditableTemplate = '<input ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="updateEntity(row)" />';
+//        $scope.cellSelectEditableTemplate = '<select ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-options="id as name for (id, name) in statuses" ng-blur="updateEntity(row)" />';
+//
+//        $scope.columnDefs = [
+//            { field: 'name', displayName: 'Name', enableCellEditOnFocus: true,
+//                editableCellTemplate: $scope.cellInputEditableTemplate, colFilterText: '' },
+//            { field: 'age', displayName: 'Age', enableCellEdit: false },
+//            { field: 'born', displayName: 'Born', enableCellEdit: false, cellFilter: 'datetime' },
+//            { field: 'status', displayName: 'Status', enableCellEditOnFocus: true,
+//                editableCellTemplate: $scope.cellSelectEditableTemplate,
+//                cellFilter: 'status'}
+//        ];
+//
+//        //Init
+//        $scope.$on('ngGridEventData', function () {
+//            $scope.gridOptions.selectRow(0, true);
+//        });
+//        //GRID/
+//
+//        //COMBO
         $scope.myComboData = [
             {name: "Old", age: 50},
             {name: "Young", age: 10}
         ];
-        //COMBO/
-
-        //Internals - do not touch
+//        //COMBO/
+//
+//        //Internals - do not touch
         $scope.$http = $http; //TODO! remove and injext services in VGBC
         angular.extend(this, new VasabiGridBaseController($scope));
-        angular.extend(this, new VasabiChartBaseController($scope));
-        angular.extend(this, new VasabiMapBaseController($scope));
+//        //angular.extend(this, new VasabiChartBaseController($scope));
+//        //angular.extend(this, new VasabiMapBaseController($scope));
 
     }]).directive('ngBlur', function () {
         return function (scope, elem, attrs) {
