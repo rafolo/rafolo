978ef2e72c307c772be9f3fced9dd56383ff763c
Rafal <rafal@pkey.pl>
2014-07-03 13:49:03 +0200

    staging generator

--- sample_app/lib/generators/staging/staging_generator.rb
+++ sample_app/lib/generators/staging/staging_generator.rb
@@ -3,5 +3,17 @@ class StagingGenerator < Rails::Generators::NamedBase
 
   def copy_staging_file
     copy_file "files/staging.rb", "config/environments/#{file_name}.rb"
+
+    str =  %Q{
+
+#TODO! Change name if !metter
+#{file_name}:
+  <<: *local_production_mysql
+  database: "#{file_name}"
+}
+
+    append_to_file 'config/database.yml', str
+
   end
+
 end
