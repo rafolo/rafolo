e8c28262729d7696d9025aa47f55b1d96bdf1566
rkukuczka <rafal@pkey.pl>
2014-07-08 08:17:56 +0200

    .

--- sample_app/script/production-src-dump.cmd
+++ sample_app/script/production-src-dump.cmd
@@ -1,4 +1,4 @@
-REM HELLO 2014-07-08 00:24:47 +0200
+REM HELLO 2014-07-08 07:46:24 +0200
 REM pushd .
 REM "c:\Program Files (x86)\Git\Cmd"\git.cmd clone  ssh://53a66617500446d6fc000abd@vasabi-rafolo.rhcloud.com/~/git/vasabi.git/
 REM cd vasabi
