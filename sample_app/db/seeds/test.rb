199ee2fcc5f1ed38b908ecbe45e99df6b972b067
rkukuczka <rafal@pkey.pl>
2014-07-09 15:30:40 +0200

    Spec..

--- sample_app/db/seeds/test.rb
+++ sample_app/db/seeds/test.rb
@@ -1,7 +1,16 @@
+#require File.expand_path("../../../config/environment.rb", __FILE__)
+
 puts 'test.rb'
 
-User.create!("email" => "rafal@pkey.pl", "name" => "Rafal", "password" => "123456", "password_confirmation" => "123456")
+User.delete_all
+user = User.create!("email" => "rafal@pkey.pl", "name" => "Rafal", "password" => "123456", "password_confirmation" => "123456")
+User.create!("email" => "olgierd.falat@gmail.com", "name" => "Olgierd", "password" => "123456", "password_confirmation" => "123456")
 
 (1..3).each do |i|
-  Alarm.create!("name" => "test#{i}", "description" => "test#{i}", "active" => false, "born" => Time.now)
+
+  user.microposts << Micropost.create(:content => "micropost#{i}")
+
+  Alarm.create!("name" => "dev#{i}", "description" => "test#{i}", "active" => false, "born" => Time.now)
 end
+
+user.save!
