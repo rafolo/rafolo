1586efe3f4428d8acd0316fa4d0fa0d83c2ad3fb
Olgierd Falat <olgierd.falat@gmail.com>
2014-07-17 11:42:26 +0200

    Update seeds files.

--- sample_app/db/seeds/development.rb
+++ sample_app/db/seeds/development.rb
@@ -5,6 +5,7 @@ puts 'development.rb'
 User.delete_all
 user = User.create!("email" => "rafal@pkey.pl", "name" => "Rafal", "password" => "123456", "password_confirmation" => "123456")
 User.create!("email" => "olgierd.falat@gmail.com", "name" => "Olgierd", "password" => "123456", "password_confirmation" => "123456")
+User.create!("email" => "vasabi-rafolo@rhcloud.com", "name" => "Default User", "password" => "123456", "password_confirmation" => "123456")
 
 (1..3).each do |i|
 
