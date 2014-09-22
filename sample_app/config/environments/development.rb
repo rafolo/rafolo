8eec79dcc0b7de6dee111f79ec3d66c83fbd0768
Olgied Falat <olgierd.falat@gmail.com>
2014-09-22 13:00:29 +0200

    Enabled public assets.

--- sample_app/config/environments/development.rb
+++ sample_app/config/environments/development.rb
@@ -48,7 +48,7 @@ SampleApp::Application.configure do
 
   #show public/assets/ after /app/assets giving them  this way the h-priority
   #clear browser data too
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
 
 end
