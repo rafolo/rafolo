d35cd0466c37fbfcb29507fc86bf476f8b4699b8
Olgied Falat <olgierd.falat@gmail.com>
2014-09-24 11:55:17 +0200

    Changed map point icons.

--- sample_app/app/controllers/map_point_controller.rb
+++ sample_app/app/controllers/map_point_controller.rb
@@ -3,8 +3,8 @@ class MapPointController < ApplicationController
   #menu2
   has_menu2 "Localisation", "/map_point/index",
             :enabled => true, :icon => 'icon-home', :order => 10,
-            :children => [ MenuItem.new('Points', '/map_point/index/#/mappoint', true, 'icon-road'),
-                           MenuItem.new('Routes', '/map_point/route/#/maproute', true, 'icon-truck')
+            :children => [ MenuItem.new('Points', '/map_point/index/#/mappoint', true, 'icon-truck'),
+                           MenuItem.new('Routes', '/map_point/route/#/maproute', true, 'icon-road')
             ]
 
   def self.enabled?
