e29e04ad4e699b46e0b5dcd72a7a33dd7d483478
rafal <rafal@pkey.pl>
2014-09-25 12:15:05 +0200

    Missings

---
+++ sample_app/app/assets/javascripts/lib/directives/vasabi-alarm.js
@@ -0,0 +1,8 @@
+angular.module("lib.directives.functional", ["lib.filters"])
+    .directive("vasabiAlarm", function () {
+        return {
+            restrict: "A",
+            transclude: true,
+            templateUrl: '/assets/lib/directives/vasabi-alarm.html'
+        };
+    });
