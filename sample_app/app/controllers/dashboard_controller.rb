1a4c14a3b33f09175b466a0e1850d2ec5b9855a5
Olgied Falat <olgierd.falat@gmail.com>
2014-09-26 16:29:09 +0200

    Added dashboard to left menu.

--- sample_app/app/controllers/dashboard_controller.rb
+++ sample_app/app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  has_menu2 "Dashboard", "/dashboard.html", :enabled => true
+  has_menu2 "Dashboard", "/dashboard", :enabled => true, :icon => 'icon-dashboard', :order => 0
 
   def index
   end
