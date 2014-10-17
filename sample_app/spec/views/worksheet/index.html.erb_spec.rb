ed5d306b29be4d087982919971cd28efecd94bd9
Olgied Falat <olgierd.falat@gmail.com>
2014-10-18 00:55:00 +0200

    Removed useless pending.

--- sample_app/spec/views/worksheet/index.html.erb_spec.rb
+++ sample_app/spec/views/worksheet/index.html.erb_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe "worksheet/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-  # before do
-  #   user = double('user')
-  #   user.stub(:name) { "test" }
-  #   user.stub(:email) { "test@example.com" }
-  #   assign(:current_user, user)
-  # end
-  # it "renders page title with 'Title | MySite'" do
-  #   render template: "worksheet/index", layout: "layouts/application"
-  #   puts rendered
-  #   rendered.should have_selector("title", text: "Title | MySite")
-  # end
+
 end
