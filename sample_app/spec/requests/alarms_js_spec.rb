055f5590d86d8704ae09fcae4970c9c6bb046c03
Olgied Falat <olgierd.falat@gmail.com>
2014-10-18 19:05:14 +0200

    Tests clean-up.

--- sample_app/spec/requests/alarms_js_spec.rb
+++ sample_app/spec/requests/alarms_js_spec.rb
@@ -11,28 +11,24 @@ describe "Alarms.js" do
     before { visit root_path }
 
     it "has page available" do
-      pending "Redirects are to costly, TODO! clean up, make work"
-      # get alarms_path
-      # current_path.should eq(alarm_index_path)
-      # response.status.should be_success
+      get alarms_path
+      response.status.should be(200)
     end
 
     it "has correct gui elements", js: true do
-      SampleApp::Application.config.show_examples = true #TODO: possible site effect for other Specs ?
+      SampleApp::Application.config.show_examples = true
 
       visit alarm_index_path
 
       current_path.should eq(alarm_index_path)
 
-
-      #should have_link('Default', href: "/credit_card/index?locale=en")
       should have_xpath('//*[@id="breadcrumbs"]/div[2]/span[1]')
       should have_content("Your alarms")
       should have_content("Alarm")
     end
 
     it "has correct UI lab links" do
-      SampleApp::Application.config.show_examples = true #TODO: possible site effect for other Specs ?
+      SampleApp::Application.config.show_examples = true
       visit alarm_index_path
       should have_xpath('//a[@href="/ui_lab/buttons.html"]')
       should have_xpath('//a[@href="/ui_lab/general.html"]')
@@ -44,19 +40,19 @@ describe "Alarms.js" do
     end
 
     it "has correct Forms link" do
-      SampleApp::Application.config.show_examples = true #TODO: possible site effect for other Specs ?
+      SampleApp::Application.config.show_examples = true
       visit alarm_index_path
       should have_xpath('//a[@href="/forms/forms.html"]')
     end
 
     it "has correct Charts link" do
-      SampleApp::Application.config.show_examples = true #TODO: possible site effect for other Specs ?
+      SampleApp::Application.config.show_examples = true
       visit alarm_index_path
       should have_xpath('//a[@href="/charts/charts.html"]')
     end
 
     it "has correct Others links" do
-      SampleApp::Application.config.show_examples = true #TODO: possible site effect for other Specs ?
+      SampleApp::Application.config.show_examples = true
       visit alarm_index_path
       should have_xpath('//a[@href="/other/wizard.html"]')
       should have_xpath('//a[@href="/other/login.html"]')
